0afa4d1e1dfae07be9541422439a8d84fd7a4bfd
Henning Dahlheim <hactar@cyberkraft.ch>
2021-12-27 23:33:58 +0100

    refactor: move client.go types.go into pkg/hakuna
    BREAKING CHANGE: import path changes from
    `hakuna-go` to `hakuna-go/pkg/hakuna`

--- pkg/hakuna/client.go
+++ pkg/hakuna/client.go
@@ -17,13 +17,13 @@ type Hakuna struct {
 	Client    http.Client
 }
 
-type Request struct {
+type request struct {
 	Method   string
 	Endpoint string
 	Body     []byte
 }
 
-type Response struct {
+type response struct {
 	StatusCode int
 	Body       []byte
 }
@@ -32,35 +32,41 @@ func New(subDomain string, token string, client http.Client) Hakuna {
 	return Hakuna{SubDomain: subDomain, Token: token, Client: client}
 }
 
-func (h Hakuna) Ping() Pong {
-	req := Request{Method: "GET", Endpoint: "/ping"}
-	res := h.request(req)
+func (h Hakuna) Ping() (Pong, error) {
+	req := request{Method: "GET", Endpoint: "/ping"}
+	res, err := h.request(req)
+	if err != nil {
+		return Pong{}, err
+	}
 
 	var pong Pong
 	if err := json.Unmarshal(res.Body, &pong); err != nil {
-		log.Fatal("Error decoding pong response")
+		return Pong{}, err
 	}
 
-	return pong
+	return pong, nil
 }
 
 func (h Hakuna) StartTimer(data StartTimerReq) (Timer, error) {
+	var timer Timer
+
 	reqBody, err := json.Marshal(&data)
 	if err != nil {
-		log.Fatal("Error creating request body")
+		return Timer{}, err
 	}
 
-	req := Request{Method: "POST", Endpoint: "/timer", Body: reqBody}
-	res := h.request(req)
-
-	var timer Timer
+	req := request{Method: "POST", Endpoint: "/timer", Body: reqBody}
+	res, err := h.request(req)
+	if err != nil {
+		return timer, err
+	}
 
 	if err := getResponeError(res); err != nil {
 		return timer, err
 	}
 
 	if err := json.Unmarshal(res.Body, &timer); err != nil {
-		return timer, errors.New("Error decoding response")
+		return timer, err
 	}
 
 	return timer, nil
@@ -74,26 +80,28 @@ func (h Hakuna) StopTimer() (TimeEntry, error) {
 
 	reqBody, err := json.Marshal(&reqData)
 	if err != nil {
-		log.Fatal("Error creating request body")
+		return TimeEntry{}, err
 	}
 
-	req := Request{Method: "PUT", Endpoint: "/timer", Body: reqBody}
-	res := h.request(req)
-
-	var timeEntry TimeEntry
+	req := request{Method: "PUT", Endpoint: "/timer", Body: reqBody}
+	res, err := h.request(req)
+	if err != nil {
+		return TimeEntry{}, err
+	}
 
 	if err := getResponeError(res); err != nil {
-		return timeEntry, err
+		return TimeEntry{}, err
 	}
 
+	var timeEntry TimeEntry
 	if err := json.Unmarshal(res.Body, &timeEntry); err != nil {
-		return timeEntry, errors.New("Error decoding response")
+		return timeEntry, errors.New("error decoding response")
 	}
 
 	return timeEntry, nil
 }
 
-func (h Hakuna) request(req Request) Response {
+func (h Hakuna) request(req request) (response, error) {
 	url := "https://" + h.SubDomain + ".hakuna.ch/api/v1" + req.Endpoint
 
 	rq, err := http.NewRequest(req.Method, url, bytes.NewBuffer(req.Body))
@@ -106,24 +114,24 @@ func (h Hakuna) request(req Request) Response {
 
 	resp, err := h.Client.Do(rq)
 	if err != nil {
-		log.Fatal("Error reading response. ", err)
+		return response{}, err
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatal("Error reading body. ", err)
+		return response{}, err
 	}
 
-	return Response{Body: body, StatusCode: resp.StatusCode}
+	return response{Body: body, StatusCode: resp.StatusCode}, nil
 }
 
-func getResponeError(res Response) error {
+func getResponeError(res response) error {
 	var apiError ResponeError
 
 	if res.StatusCode >= 300 {
 		if err := json.Unmarshal(res.Body, &apiError); err != nil {
-			return errors.New("Error decoding response")
+			return errors.New("error decoding response")
 		}
 		return errors.New(apiError.Message)
 	}
