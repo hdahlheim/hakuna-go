e41f8f80c47056c64443dd99c619d77507090531
Henning Dahlheim <hactar@cyberkraft.ch>
2022-01-02 22:13:58 +0100

    chore: Add MPL-2.0 License

--- internal/lib/ternary.go
+++ internal/lib/ternary.go
@@ -1,3 +1,11 @@
+/*
+Copyright © 2021 Henning Dahlheim <hactar@cyberkraft.ch>
+
+This Source Code Form is subject to the terms of the Mozilla Public
+License, v. 2.0. If a copy of the MPL was not distributed with this
+file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+*/
 package lib
 
 func Ternary(cond bool, truthy interface{}, falsy interface{}) interface{} {
